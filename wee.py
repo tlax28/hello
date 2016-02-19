dfbae35e23333bd707bf41947131b39c399eeb79
lihweilihwei <lihweilihwei@gmail.com>
2016-02-20 00:28:36 +0800

    'toPowerOf' function
    Replaced the 'returnSq' function with the 'toPowerOf' function. Takes in a number and a power and return the number raised to the specified power.

--- wee.py
+++ wee.py
@@ -1,8 +1,8 @@
 def addNumbers(num1, num2):
     return num1+num2
 
-def returnSq(num1):
-    return num1^2
+def toPowerOf(num1, power):
+    return num1^power
 
 def newSn(num1):
     return (num1+1)
